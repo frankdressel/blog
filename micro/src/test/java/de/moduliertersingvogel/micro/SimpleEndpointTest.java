f5edc8452a41e9ae20149cea06e522cd2c5c8022
frankdressel <frankdressel@users.noreply.github.com>
2018-01-15 22:50:33 +0100

    Updated version of okhttp.

--- micro/src/test/java/de/moduliertersingvogel/micro/SimpleEndpointTest.java
+++ micro/src/test/java/de/moduliertersingvogel/micro/SimpleEndpointTest.java
@@ -8,9 +8,9 @@ import org.apache.meecrowave.testing.ConfigurationInject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 @MeecrowaveConfig /*(some config)*/
 public class SimpleEndpointTest {
