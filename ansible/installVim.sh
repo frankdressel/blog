34799c91803b31a69a061f3f4246d40b0bbbc44a
Frank Dressel <frankdressel@users.noreply.github.com>
2020-04-05 21:46:48 +0000

    Updated to python3

--- ansible/installVim.sh
+++ ansible/installVim.sh
@@ -1,4 +1,4 @@
 cd /tmp/vim
-./configure --with-features=huge --enable-multibyte --enable-rubyinterp --enable-pythoninterp --with-python-config-dir=$(bash python-config --configdir) --enable-perlinterp --enable-gui=gtk2 --enable-cscope --prefix=/usr
+./configure --with-features=huge --enable-multibyte --enable-rubyinterp --enable-python3interp --with-python3-config-dir=$(bash python3-config --configdir) --enable-perlinterp --enable-gui=gtk2 --enable-cscope --prefix=/usr
 make VIMRUNTIMEDIR=/usr/share/vim/vim82
 make install
