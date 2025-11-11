e4fc352bffb4f47dd79285d412d3fc97ce9e3c8c
Frank Dressel <frankdressel@users.noreply.github.com>
2025-11-11 23:33:01 +0100

    Updated vim install

--- ansible/installVim.sh
+++ ansible/installVim.sh
@@ -1,4 +1,6 @@
 cd /tmp/vim
+MAJOR=$(grep "MAJOR = " Makefile | sed "s/MAJOR = //g")
+MINOR=$(grep "MINOR = " Makefile | sed "s/MINOR = //g")
 ./configure --with-features=huge --enable-multibyte --enable-rubyinterp --enable-python3interp --with-python3-config-dir=$(bash python3-config --configdir) --enable-perlinterp --enable-gui=gtk2 --enable-cscope --prefix=/usr
-make VIMRUNTIMEDIR=/usr/share/vim/vim82
+make VIMRUNTIMEDIR=/usr/share/vim/vim$MAJOR$MINOR
 make install
